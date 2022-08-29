5a5df03e8221b4e0f7e36e8ac35dcb4ac510a8f8
Matheus Trojan <matheustrojan23@gmail.com>
2022-08-29 16:03:48 -0300

    Criadas funções dos botões de som

--- 13_aluramidi-iniciando-javascript/main.js
+++ 13_aluramidi-iniciando-javascript/main.js
@@ -1,12 +1,18 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();  
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();  
 }
 
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
+
+    const tecla = listaDeTeclas[contador];    
+    const instrumento = tecla.classList[1];
+    //        ------- template string
+    const idAudio = `#som_${instrumento}`    
+
+    //          ----função anonima
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
 }
-
-
-
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
