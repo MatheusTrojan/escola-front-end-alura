dbb869d7f02acfaa7f54da009c459a3aa96a75fe
Matheus Trojan <matheustrojan23@gmail.com>
2022-08-29 12:13:19 -0300

    Iniciando funções

--- 13_aluramidi-iniciando-javascript/main.js
+++ 13_aluramidi-iniciando-javascript/main.js
@@ -1,1 +1,12 @@
-document.querySelector('tecla_pom');
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();  
+}
+
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
