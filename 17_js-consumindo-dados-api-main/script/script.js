2c6d672cb5adabdb1f55de82fbe9255cc4c2bebb
Matheus Trojan <matheustrojan23@gmail.com>
2022-10-11 14:25:49 -0300

    finalizando curso de consumo de api

--- 17_js-consumindo-dados-api-main/script/script.js
+++ 17_js-consumindo-dados-api-main/script/script.js
@@ -1,14 +1,27 @@
 async function buscaEndereco(cep){
+    var mensagemErro = document.getElementById("erro")
+    mensagemErro.innerHTML = "";
     try {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var consultaCEPConvertida = await consultaCEP.json();
         if (consultaCEPConvertida.erro) {
             throw Error("CEP não existente!");
         }
+
+        var cidade = document.getElementById("cidade");
+        var logradouro = document.getElementById("endereco");
+        var bairro = document.getElementById("bairro")
+        var estado = document.getElementById("estado");
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        bairro.value = consultaCEPConvertida.bairro;
+        estado.value = consultaCEPConvertida.uf;
+
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch (erro) {
-        console.log(erro)
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
     }
 }
 
