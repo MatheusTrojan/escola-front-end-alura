17400d1cc97363e3806de753fdf31d1ff1e1be14
Matheus Trojan <matheustrojan23@gmail.com>
2022-10-11 12:21:08 -0300

    Buscando CEP com focusout no input

--- 17_js-consumindo-dados-api-main/script/script.js
+++ 17_js-consumindo-dados-api-main/script/script.js
@@ -1,11 +1,43 @@
-var consultaCEP = fetch("https://viacep.com.br/ws/01001000/json/")
-.then(resposta => resposta.json())
-.then(r => {
-    if (r.erro) {
-        throw Error("Esse CEP não existe")
-    } else
-        console.log(r)})
-.catch(erro => console.log(erro))
-.finally(mensagem => console.log("Processamento concluído!"));
+async function buscaEndereco(cep){
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error("CEP não existente!");
+        }
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        console.log(erro)
+    }
+}
 
-console.log(consultaCEP)
+var cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value))
+
+// .then(resposta => resposta.json())
+// .then(r => {
+//     if (r.erro) {
+//         throw Error("Esse CEP não existe")
+//     } else
+//         console.log(r)})
+// .catch(erro => console.log(erro))
+// .finally(mensagem => console.log("Processamento concluído!"));
+
+
+
+// <<<< PARA FAZER VÁRIAS REQUISIÇÕES >>>>
+// let ceps = ["01001000", "01001001"];
+// let conjuntosCEPs = ceps.map(valores => buscaEndereco(valores));
+// Promise.all(conjuntosCEPs).then(respostas => console.log(respostas));
+
+
+//<<<<<<<<<<<<<<<<<<<<TESTANDO FILME>>>>>>>>>>>>>>>>>>>>>
+// async function buscaFilme(){
+
+//     var filme = await fetch("https://imdb-api.com/en/API/SearchMovie/k_o545bjp7/inception 2010")
+//     var filmeEditado = await filme.json();
+//     console.log(filmeEditado.results[0].image)
+// }
+// buscaFilme()
+//<<<<<<<<<<<<<<<<<<<<TESTANDO FILME>>>>>>>>>>>>>>>>>>>>>
