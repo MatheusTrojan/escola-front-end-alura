aeb9452f04f1449a7152d0939dd4c36cc6499d2b
Matheus Trojan <matheustrojan23@gmail.com>
2022-10-31 11:11:02 -0300

    registra aula sobre console

---
+++ 21_js-basico/01.js
@@ -0,0 +1,9 @@
+// const numero; >> Erro de declaração da constante (const não pode ficar vazio)
+
+
+// console.log >> exibe informações no console
+// console.error() >> para exibir mensagens de erro;
+// console.table() >> para visualizar de forma mais organizada informações tabulares;
+// console.time() e console.timeEnd() >> para temporizar período que uma operação de código leva para ser iniciada e concluída;
+// console.trace() >> para exibir a stacktrace de todos os pontos (ou seja, os arquivos chamados) por onde o código executado passou durante a execução.
+
