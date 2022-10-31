78f5c333ac769cfe5879ef9dd1a5f35c7b6826ff
Matheus Trojan <matheustrojan23@gmail.com>
2022-10-31 15:23:40 -0300

    anota sobre tipos de funções

--- 21_js-basico/01.js
+++ 21_js-basico/01.js
@@ -17,3 +17,32 @@
 // template string >> substitui a concatenação utilizando várias vezes o simbolo de adição e variáveis, criando tudo na mesma frase com crase>>> `texto texto ${variavel} texto texto`
 
 
+// três formas de escrever funções: 
+
+//  1- function NomeDaFunção(parâmetros) {
+//           função 
+//      }
+
+//  É possivel passar uma função como parâmetro da outra
+
+// 2 - expressao de função (geralmente são funções anonimas) // não podemos chamar ela antes de declarar
+// se eu chamasse um console.log(soma) antes de declarar a função daria erro
+// const soma = function(num1, num2) {return num1 + num2}
+
+// 3 - arrow function 
+// const testeArrow = parametroNome => `meu nome é ${parametroNome}`
+// const soma = (num1, num2) => num1 + num2
+
+// 3.1 - arrow function com + de uma linha de codigo
+
+const  somaNumerosPequenos = (num1, num2) => {
+    if (num1 > 10 || num2 > 10) {
+        return "somente numeros de 1 a 9"
+    } else {
+        return num1 + num2
+    }
+}
+
+console.log(somaNumerosPequenos(5, 1))
+
+// Hoisting: arrow function se comporta como expressao
