cdec41d1d6f7a977ae5761d5726c819c2e5ae48a
Matheus Trojan <matheustrojan23@gmail.com>
2022-10-31 11:54:34 -0300

    comparações

--- 21_js-basico/01.js
+++ 21_js-basico/01.js
@@ -7,3 +7,9 @@
 // console.time() e console.timeEnd() >> para temporizar período que uma operação de código leva para ser iniciada e concluída;
 // console.trace() >> para exibir a stacktrace de todos os pontos (ou seja, os arquivos chamados) por onde o código executado passou durante a execução.
 
+
+// comparação implícita >> == compara o valor ; === compara o valor e tipo do dado
+
+// comparação explícita >> operador ternário >> console.log(idade >= idadeMinima ? "pode beber" : "nao pode beber")
+//                                                              condição             if true          if false
+
