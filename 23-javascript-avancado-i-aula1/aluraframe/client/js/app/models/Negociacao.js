9356de8dfa225548488a389ba50fec0f6a7c03be
Matheus Trojan <matheustrojan23@gmail.com>
2022-11-22 11:04:02 -0300

    inicia scripts de classe construtora imutavel

---
+++ 23-javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
@@ -0,0 +1,31 @@
+class Negociacao {
+    
+    constructor(data, quantidade, valor) {
+
+        // underline na frente do nome como CONVENÇÃO de que o valor não deve ser alterado/acessado
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this) // congela o objeto, o tornando imutável
+    }
+
+    //metodos "acessadores" com o get >> apenas a classe acessa os dados
+    // passando para GETTER, se torna apenas para leitura dos dados
+    //mesmo que ainda seja possivel alterar o dados utilizando _, dificulta o acesso para quem tentar colocar uma quantidade diferente
+    get volume() {
+        return this._quantidade * this._valor
+    }
+
+    get data() {
+        //retornamos um novo objeto que pega uma nova data baseada na que foi passada na Negociacao
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+}
