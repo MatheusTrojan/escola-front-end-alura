c1e80ab9c0410d8e234071377779cde086d52c15
Matheus Trojan <matheustrojan23@gmail.com>
2022-11-25 11:37:24 -0300

    mostra mensagem de cadastro da negociacao

--- 23-javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ 23-javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,17 +7,28 @@ class negociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        
         this._listaNegociacoes = new ListaNegociacoes();
+
         this._negociacoesView = new NegociacoesView($("#negociacoesView"));
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = new Mensagem()
+        this._mensagemView = new MensagemView($("#mensagemView"))
+        this._mensagemView.update(this._mensagem)
 
     }
 
     adiciona(event) {
 
         event.preventDefault()
+
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = "Negociação adicionada com sucesso!"
+        this._mensagemView.update(this._mensagem)
+
         this._limpaFormulario()
     }
 
