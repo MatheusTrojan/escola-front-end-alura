ae6b17c5265c046a993a71bd7f55e034646fd7a5
Matheus Trojan <matheustrojan23@gmail.com>
2022-11-29 10:12:29 -0300

    inicia utilização de proxy

--- 23-javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ 23-javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,58 +1,56 @@
-class negociacaoController {
-
+class NegociacaoController {
+    
     constructor() {
-        // quando jogamos o querySelector para uma variavel, ele perde o contexto do "document", o THIS não apontaria mais para o "document"... então usamos o BIND para o $ fazer a referência ao "document" novamente (parecido com Jquery)
-        let $ = document.querySelector.bind(document)
-
-        this._inputData = $("#data");
-        this._inputQuantidade = $("#quantidade");
-        this._inputValor = $("#valor");
         
-        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
-
-        this._negociacoesView = new NegociacoesView($("#negociacoesView"));
+                // quando jogamos o querySelector para uma variavel, ele perde o contexto do "document", o THIS não apontaria mais para o "document"... então usamos o BIND para o $ fazer a referência ao "document" novamente (parecido com Jquery)
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+        
+        // this._listaNegociacoes = new ListaNegociacoes(model => 
+        //     this._negociacoesView.update(model));
+        
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
-
-        this._mensagem = new Mensagem()
-        this._mensagemView = new MensagemView($("#mensagemView"))
-        this._mensagemView.update(this._mensagem)
-
+        
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
+        
     }
-
+    
     adiciona(event) {
-
-        event.preventDefault()
-
+        
+        event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-
-        this._mensagem.texto = "Negociação adicionada com sucesso!"
+        
+        this._mensagem.texto = 'Negociação adicionada com sucesso';
         this._mensagemView.update(this._mensagem);
-
-        this._limpaFormulario()
+        
+        this._limpaFormulario();   
     }
-
+    
     apaga() {
-
+        
         this._listaNegociacoes.esvazia();
-
-        this._mensagem.texto = "Lista de negociações apagada com sucesso!"
+         
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
         this._mensagemView.update(this._mensagem);
     }
-
     _criaNegociacao() {
-
+        
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value
-        );
+            this._inputValor.value);    
     }
-
+    
     _limpaFormulario() {
-
-        this._inputData.value = "";
+     
+        this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
-        this._inputData.focus();
+        this._inputData.focus();   
     }
 }
--- 23-javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ 23-javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,27 +1,23 @@
 class ListaNegociacoes {
 
-    constructor(armadilha) {
-
+    constructor() {
+        
         this._negociacoes = [];
-        this._armadilha = armadilha;
     }
 
     adiciona(negociacao) {
 
-        this._negociacoes.push(negociacao);
-        this._armadilha(this);
-
+        this._negociacoes.push(negociacao);        
     }
 
     get negociacoes() {
-
+        
         return [].concat(this._negociacoes);
     }
 
     esvazia() {
 
         this._negociacoes = [];
-        this._armadilha(this);
         // Reflect.apply(this._armadilha, this._contexto, [this])
         // tivemos que criar esse reflect apply por causa do escopo do this, onde no NegociacaoController ele estava sendo chamado para Lista Negociação quando queriamos que fosse para o controller >>> this._listaNegociacoes = new ListaNegociacoes(this, function(model) <<<
         // deixamos de utlizar o Reflect pois a Arrow Function resolveu nosso problema devido seu escopo léxico do seu this, ou seja, não muda de acordo com o contexto
