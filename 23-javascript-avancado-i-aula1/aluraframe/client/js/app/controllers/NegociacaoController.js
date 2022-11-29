8ebbd865a6ba1c140e3a13175d1a161aa823deb3
Matheus Trojan <matheustrojan23@gmail.com>
2022-11-28 21:27:16 -0300

    utiliza Reflect API para mudar contexto do this no update

--- 23-javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ 23-javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -8,7 +8,10 @@ class negociacaoController {
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
         
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(this, function(model) {
+
+            this._negociacoesView.update(model);
+        });
 
         this._negociacoesView = new NegociacoesView($("#negociacoesView"));
         this._negociacoesView.update(this._listaNegociacoes);
@@ -24,7 +27,6 @@ class negociacaoController {
         event.preventDefault()
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = "Negociação adicionada com sucesso!"
         this._mensagemView.update(this._mensagem);
@@ -35,7 +37,6 @@ class negociacaoController {
     apaga() {
 
         this._listaNegociacoes.esvazia();
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = "Lista de negociações apagada com sucesso!"
         this._mensagemView.update(this._mensagem);
--- 23-javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ 23-javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,13 +1,17 @@
 class ListaNegociacoes {
 
-    constructor() {
+    constructor(contexto, armadilha) {
 
-        this._negociacoes = []
+        this._negociacoes = [];
+        this._armadilha = armadilha;
+        this._contexto = contexto;
     }
 
     adiciona(negociacao) {
 
         this._negociacoes.push(negociacao);
+        // this._armadilha(this);
+        Reflect.apply(this._armadilha, this._contexto, [this])
     }
 
     get negociacoes() {
@@ -18,5 +22,8 @@ class ListaNegociacoes {
     esvazia() {
 
         this._negociacoes = [];
+        // this._armadilha(this);
+        Reflect.apply(this._armadilha, this._contexto, [this])
+        // tivemos que criar esse reflect apply por causa do escopo do this, onde no NegociacaoController ele estava sendo chamado para Lista Negociação quando queriamos que fosse para o controller >>> this._listaNegociacoes = new ListaNegociacoes(this, function(model) <<<
     }
 }
