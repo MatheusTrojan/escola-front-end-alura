ee0d8cfd5528c04b9374a3a52b3b9ef3a2d4b4e1
Matheus Trojan <matheustrojan23@gmail.com>
2022-11-29 14:21:21 -0300

    melhora o codigo do proxyfactory

--- 23-javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ 23-javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -11,7 +11,8 @@ class NegociacaoController {
         this._listaNegociacoes = ProxyFactory.create(
             new ListaNegociacoes(),
             ["adiciona", "esvazia"],
-            (model) => this._negociacoesView.update(model))
+            (model) => this._negociacoesView.update(model)
+        )
 
         // this._listaNegociacoes = new ListaNegociacoes(model => 
         //     this._negociacoesView.update(model)); SUBSTITUIDO PELO PROXY
@@ -19,7 +20,12 @@ class NegociacaoController {
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
         
-        this._mensagem = new Mensagem();
+        this._mensagem = ProxyFactory.create(
+            new Mensagem(),
+            ["texto"], 
+            (model) => this._mensagemView.update(model)
+        )
+
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);
         
@@ -28,21 +34,17 @@ class NegociacaoController {
     adiciona(event) {
         
         event.preventDefault();
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        
-        this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
-        
+        this._listaNegociacoes.adiciona(this._criaNegociacao());        
+        this._mensagem.texto = 'Negociação adicionada com sucesso';        
         this._limpaFormulario();   
     }
     
     apaga() {
         
-        this._listaNegociacoes.esvazia();
-         
+        this._listaNegociacoes.esvazia();         
         this._mensagem.texto = 'Negociações apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
     }
+    
     _criaNegociacao() {
         
         return new Negociacao(
--- 23-javascript-avancado-i-aula1/aluraframe/client/js/app/services/ProxyFactory.js
+++ 23-javascript-avancado-i-aula1/aluraframe/client/js/app/services/ProxyFactory.js
@@ -1,12 +1,20 @@
+            //get vai ser sempre chamado toda vez que eu tentar ler qualquer propriedade do objeto
+            // quando chamado, me da o TARGET (referencia ao objeto original que esta sendo encapsulado pelo proxy);
+            // PROP -> a propriedade que está sendo acessada
+            // RECEIVER -> uma referencia ao proprio proxy
+
+            // Quando executamos a TRAP(ARMADILHA), temos que dar o valor retornado após ter interceptado a propriedade de leitura
+
+            //ARGUMENTS é um parametro acessível em qualquer função que me da acesso à todos os parâmetros da função 
 class ProxyFactory {
 
     static create(objeto, props, acao) {
 
-        return new Proxy(new ListaNegociacoes(), {
+        return new Proxy(objeto, {
 
             get(target, prop, receiver) {
 
-                if(props.includes(prop) && typeof(target[prop]) == typeof(Function)) {
+                if(props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
 
                     return function() {
 
@@ -19,15 +27,20 @@ class ProxyFactory {
                 }
 
                 return Reflect.get(target, prop, receiver);
+            },
+
+            set (target, prop, value, receiver) {
+                if(props.includes(prop)) {
+                    target[prop] = value;
+                    acao(target);
+                }
+                return Reflect.set(target, prop, value, receiver);
             }
-            //get vai ser sempre chamado toda vez que eu tentar ler qualquer propriedade do objeto
-            // quando chamado, me da o TARGET (referencia ao objeto original que esta sendo encapsulado pelo proxy);
-            // PROP -> a propriedade que está sendo acessada
-            // RECEIVER -> uma referencia ao proprio proxy
-
-            // Quando executamos a TRAP(ARMADILHA), temos que dar o valor retornado após ter interceptado a propriedade de leitura
-
-            //ARGUMENTS é um parametro acessível em qualquer função que me da acesso à todos os parâmetros da função 
         });
+    }
+
+    static _ehFuncao(func) {
+
+        return typeof(func) == typeof(Function)
     }
 }
