a4e366f50be2ad6e8ccd2f4b5d9a4b14d9602441
Matheus Trojan <matheustrojan23@gmail.com>
2022-11-30 15:32:13 -0300

    cria codigo para ordenar a lista de negociações

--- 23-javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ 23-javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,11 +7,12 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._ordemAtual = '';  
 
         this._listaNegociacoes = new Bind(
             new ListaNegociacoes(),
             new NegociacoesView($('#negociacoesView')),
-            "adiciona", "esvazia")
+            "adiciona", "esvazia", "ordena", "inverteOrdem")
         
         this._mensagem = new Bind(
             new Mensagem(),
@@ -110,5 +111,14 @@ class NegociacaoController {
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
         this._inputData.focus();   
+    }
+
+    ordena(coluna) {
+        if(this._ordemAtual == coluna) {
+            this._listaNegociacoes.inverteOrdem();
+        } else {
+            this._listaNegociacoes.ordena((a, b) => a[coluna] - b[coluna]);    
+        }
+        this._ordemAtual = coluna;
     }
 }
--- 23-javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ 23-javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -26,4 +26,12 @@ class ListaNegociacoes {
     get volumeTotal() {
         return this._negociacoes.reduce((total, n) => total + n.volume, 0.0);
      }
+
+    ordena(criterio) {
+        this._negociacoes.sort(criterio);        
+    }
+
+    inverteOrdem() {
+        this._negociacoes.reverse();
+    }
 }
