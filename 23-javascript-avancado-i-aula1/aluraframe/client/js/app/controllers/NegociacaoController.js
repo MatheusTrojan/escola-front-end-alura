d178e7685498d7584dec380d645439394f54e925
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-01 15:17:50 -0300

    mostra na tela e apaga as negociacoes linkadas ao indexedDB

--- 23-javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ 23-javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -18,6 +18,19 @@ class NegociacaoController {
             new Mensagem(),
             new MensagemView($('#mensagemView')),
             "texto")
+
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.listaTodos())
+            .then(negociacoes => 
+                negociacoes.forEach(negociacao => 
+                    this._listaNegociacoes.adiciona(negociacao)
+                ))
+            .catch(erro => {
+                console.log(erro);
+                this._mensagem.texto = error;
+            })
     }
     
     adiciona(event) {
@@ -105,16 +118,24 @@ class NegociacaoController {
     
     apaga() {
         
-        this._listaNegociacoes.esvazia();         
-        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.apagaTodos())
+            .then(mensagem => {
+                this._mensagem.texto = mensagem;
+                this._listaNegociacoes.esvazia();         
+            })
+
+
     }
     
     _criaNegociacao() {
         
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value);    
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value));    
     }
     
     _limpaFormulario() {
