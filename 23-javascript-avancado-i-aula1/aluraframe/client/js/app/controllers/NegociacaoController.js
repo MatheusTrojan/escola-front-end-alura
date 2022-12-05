0ab3de92d2a09e995e675860e5d4ee3500d2933e
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-05 11:49:25 -0300

    utiliza 'some' para filtrar negociacoes duplicadas e não permitir importação

--- 23-javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ 23-javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -62,7 +62,9 @@ class NegociacaoController {
         .obterNegociacoes()
         .then(negociacoes => 
             negociacoes.filter(negociacao => 
-                this._listaNegociacoes.negociacoes.indexOf(negociacao) == -1)
+                !this._listaNegociacoes.negociacoes.some(negociacaoExistente => 
+                    JSON.stringify(negociacao) == JSON.stringify(negociacaoExistente))
+                )
         )
         .then(negociacoes => {negociacoes.forEach(negociacao => 
             this._listaNegociacoes.adiciona(negociacao));
