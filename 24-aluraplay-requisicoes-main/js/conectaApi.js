ff3a930d0482cdb104edd01286bcba3744446bb0
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-06 15:24:51 -0300

    inicia conexao com api

---
+++ 24-aluraplay-requisicoes-main/js/conectaApi.js
@@ -0,0 +1,11 @@
+async function listaVideos() {
+
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoConvertida = await conexao.json();
+    
+    return conexaoConvertida;
+}
+
+export const conectaApi = {
+    listaVideos
+}
