ddde7d85c8326b90b5d6790ef0d9925a2993b6c1
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-07 15:31:26 -0300

    inicia pagina de adicionar videos

---
+++ 24-aluraplay-requisicoes-main/js/criarVideo.js
@@ -0,0 +1,18 @@
+import { conectaApi } from "./conectaApi";
+
+const formulario = document.querySelector("[data-formulario]");
+
+async function criarVideo(evento) {
+    evento.preventDefault();
+
+    const imagem = document.querySelector("[data-imagem]").value;
+    const url = document.querySelector("[data-url]").value;
+    const titulo = document.querySelector("[data-titulo]").value;
+    const descricao = Math.floor(Math.random() * 10).toString();
+
+    await conectaApi.criaVideo(titulo, descricao, url, imagem);
+
+    window.location.href = "../pages/envio-concluido.html";
+}
+
+formulario.addEventListener("submit", evento => criarVideo(evento))
