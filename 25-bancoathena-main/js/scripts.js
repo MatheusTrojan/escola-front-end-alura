674073d969c407b44f47904d368c671412378424
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-08 19:02:33 -0300

    cria validação de idade e cpf

---
+++ 25-bancoathena-main/js/scripts.js
@@ -0,0 +1,17 @@
+import ehUmCPF from "./valida-cpf.js";
+import ehMaiorDeIdade from "./valida-idade.js";
+
+const camposForm = document.querySelectorAll("[required]");
+
+camposForm.forEach((campo) => {
+    campo.addEventListener("blur", () => verificaCampo(campo))
+})
+
+function verificaCampo(campo) {
+    if (campo.name == "cpf" && campo.value.length >= 11) {
+        ehUmCPF(campo);
+    }
+    if (campo.name == "aniversario" && campo.value != "") {
+        ehMaiorDeIdade(campo);
+    }
+}
