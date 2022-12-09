b3fa78ffdd134a7d3940813a373ed3746fd7a1db
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-09 11:46:55 -0300

    salva os dados do formulario no localStorage

--- 25-bancoathena-main/js/scripts.js
+++ 25-bancoathena-main/js/scripts.js
@@ -2,6 +2,23 @@ import ehUmCPF from "./valida-cpf.js";
 import ehMaiorDeIdade from "./valida-idade.js";
 
 const camposForm = document.querySelectorAll("[required]");
+const formulario = document.querySelector("[data-formulario]");
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const listaRespostas = {
+        "nome": e.target.elements["nome"].value,
+        "email": e.target.elements["email"].value,
+        "rg": e.target.elements["rg"].value,
+        "cpf": e.target.elements["cpf"].value,
+        "aniversario": e.target.elements["aniversario"].value
+    }
+
+    localStorage.setItem("cadastro", JSON.stringify(listaRespostas));
+
+    window.location.href = "./abrir-conta-form-2.html"
+})
 
 camposForm.forEach((campo) => {
     campo.addEventListener("blur", () => verificaCampo(campo));
