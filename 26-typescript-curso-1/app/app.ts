73b352427a3cb482e47b28a3bbb22279924f53b3
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-11 19:48:58 -0300

    cria o link entre o botao de submit e os dados do form

--- 26-typescript-curso-1/app/app.ts
+++ 26-typescript-curso-1/app/app.ts
@@ -1,4 +1,9 @@
-import { Negociacao } from "./models/negociacao.js";
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
-const negociacao = new Negociacao(new Date(), 20, 100);
+const controller = new NegociacaoController();
+const form = document.querySelector(".form");
 
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    controller.adiciona();
+});
