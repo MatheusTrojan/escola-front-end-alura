13cf4be30d63a1812bd244dac33323e1672c3dc2
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-13 11:18:58 -0300

    utiliza programacao defensiva para proteger a data

--- 26-typescript-curso-1/app/models/negociacao.ts
+++ 26-typescript-curso-1/app/models/negociacao.ts
@@ -1,10 +1,15 @@
 export class Negociacao {
 
     constructor(
-        public readonly data: Date, 
+        private _data: Date, 
         public readonly quantidade: number, 
         public readonly valor: number
     ) {}
+
+    get data(): Date {
+        const CopiaData = new Date(this._data.getTime())
+        return CopiaData;
+    }
 
     get volume(): number {
         return this.quantidade * this.valor
--- 26-typescript-curso-1/dist/js/models/negociacao.js
+++ 26-typescript-curso-1/dist/js/models/negociacao.js
@@ -1,8 +1,12 @@
 export class Negociacao {
-    constructor(data, quantidade, valor) {
-        this.data = data;
+    constructor(_data, quantidade, valor) {
+        this._data = _data;
         this.quantidade = quantidade;
         this.valor = valor;
+    }
+    get data() {
+        const CopiaData = new Date(this._data.getTime());
+        return CopiaData;
     }
     get volume() {
         return this.quantidade * this.valor;
