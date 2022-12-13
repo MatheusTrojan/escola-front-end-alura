347d5b8fd3060ad2680a5efa9b3f86dff0028080
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-13 11:10:32 -0300

    inicia refatoração do codigo

--- 26-typescript-curso-1/dist/js/models/negociacao.js
+++ 26-typescript-curso-1/dist/js/models/negociacao.js
@@ -1,19 +1,10 @@
 export class Negociacao {
     constructor(data, quantidade, valor) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
-    get data() {
-        return this._data;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
     get volume() {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
