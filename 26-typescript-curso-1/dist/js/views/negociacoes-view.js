6975fff870bb88d91003dab8c60acedf5b201ab2
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-14 11:12:57 -0300

    cria view para exibir tabela

---
+++ 26-typescript-curso-1/dist/js/views/negociacoes-view.js
@@ -0,0 +1,24 @@
+export class NegociacoesView {
+    constructor(seletor) {
+        this.elemento = document.querySelector(seletor);
+    }
+    template() {
+        return `
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                        <th>VOLUME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        `;
+    }
+    update() {
+        this.elemento.innerHTML = this.template();
+    }
+}
