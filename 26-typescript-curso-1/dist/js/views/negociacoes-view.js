4cb5cb52da71873c79f3fd1a3edaf85ac8acf189
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-14 11:32:01 -0300

    mostra as negociacoes na tela

--- 26-typescript-curso-1/dist/js/views/negociacoes-view.js
+++ 26-typescript-curso-1/dist/js/views/negociacoes-view.js
@@ -2,7 +2,7 @@ export class NegociacoesView {
     constructor(seletor) {
         this.elemento = document.querySelector(seletor);
     }
-    template() {
+    template(model) {
         return `
             <table class="table table-hover table-bordered">
                 <thead>
@@ -10,15 +10,25 @@ export class NegociacoesView {
                         <th>DATA</th>
                         <th>QUANTIDADE</th>
                         <th>VALOR</th>
-                        <th>VOLUME</th>
                     </tr>
                 </thead>
                 <tbody>
+                    ${model.lista().map(negociacao => {
+            return `
+                            <tr>
+                                <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                                <td>${negociacao.quantidade}</td>
+                                <td>${negociacao.valor}</td>
+                            </tr>
+                        `;
+        }).join("")}
                 </tbody>
             </table>
         `;
     }
-    update() {
-        this.elemento.innerHTML = this.template();
+    update(model) {
+        const template = this.template(model);
+        console.log(template);
+        this.elemento.innerHTML = template;
     }
 }
