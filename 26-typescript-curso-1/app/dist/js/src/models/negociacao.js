0a11219015d2752cac52711bb4ce0b5c35ea3106
Matheus Trojan <matheustrojan23@gmail.com>
2022-12-15 14:17:40 -0300

    inicia 3 modulo e reorganiza arquivos

---
+++ 26-typescript-curso-1/app/dist/js/src/models/negociacao.js
@@ -0,0 +1,21 @@
+export class Negociacao {
+    constructor(_data, quantidade, valor) {
+        this._data = _data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+    get data() {
+        const CopiaData = new Date(this._data.getTime());
+        return CopiaData;
+    }
+    get volume() {
+        return this.quantidade * this.valor;
+    }
+    static criaDe(dataString, quantidadeString, valorString) {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ","));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
+    }
+}
---
+++ 26-typescript-curso-1/app/dist/js/src/views/negociacoes-view.js
@@ -0,0 +1,30 @@
+import { View } from "./view.js";
+export class NegociacoesView extends View {
+    template(model) {
+        return `
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${model.lista().map(negociacao => {
+            return `
+                            <tr>
+                                <td>${this.formataData(negociacao.data)}</td>
+                                <td>${negociacao.quantidade}</td>
+                                <td>${negociacao.valor}</td>
+                            </tr>
+                        `;
+        }).join("")}
+                </tbody>
+            </table>
+        `;
+    }
+    formataData(data) {
+        return new Intl.DateTimeFormat().format(data);
+    }
+}
